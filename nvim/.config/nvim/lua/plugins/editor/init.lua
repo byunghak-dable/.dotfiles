37bcc5238cb75c8ae33847425ab6d7deb7839792
byunghak082 <byunghak@dable.io>
2023-09-22 00:07:14 +0900

    [SORT] split files

--- nvim/.config/nvim/lua/plugins/editor/init.lua
+++ nvim/.config/nvim/lua/plugins/editor/init.lua
@@ -5,35 +5,6 @@ return {
 		config = true,
 	},
 	{
-		"kevinhwang91/nvim-hlslens",
-		event = "InsertEnter",
-		config = true,
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		event = "VeryLazy",
-		opts = { show_current_context = true },
-	},
-	{
-		"RRethy/vim-illuminate",
-		event = { "BufReadPost", "BufNewFile" },
-		opts = {
-			large_file_cutoff = 2000,
-			large_file_overrides = { providers = { "lsp" } },
-		},
-		config = function(_, opts)
-			require("illuminate").configure(opts)
-
-			for _, suffix in pairs({ "Text", "Read", "Write" }) do
-				vim.api.nvim_set_hl(0, "IlluminatedWord" .. suffix, { bg = "#444c56" })
-			end
-		end,
-		keys = {
-			{ "]r", function() require("illuminate").goto_next_reference(false) end },
-			{ "[r", function() require("illuminate").goto_prev_reference(false) end },
-		},
-	},
-	{
 		"echasnovski/mini.surround",
 		event = "InsertEnter",
 		opts = {
