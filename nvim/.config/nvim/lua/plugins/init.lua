2806b798dea4238e29d18642b6a64ffd2162d9ba
byunghak082 <byunghak@dable.io>
2023-09-29 01:08:25 +0900

    [FIX] run to build neovim

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -2,6 +2,6 @@ return {
 	{
 		"iamcco/markdown-preview.nvim",
 		ft = "markdown",
-		run = function() vim.fn["mkdp#util#install"]() end,
+		build = function() vim.fn["mkdp#util#install"]() end,
 	},
 }
