38bdcd56e199805096fb1c5c1b3bf6ec557d8503
byunghak082 <byunghak@dable.io>
2023-09-30 15:44:54 +0900

    [DEL] replace go import with null-ls

--- nvim/.config/nvim/lua/plugins/lsp/go.lua
+++ nvim/.config/nvim/lua/plugins/lsp/go.lua
@@ -2,15 +2,6 @@ return {
 	"ray-x/go.nvim",
 	ft = { "go", "gomod" },
 	event = "CmdlineEnter",
-	config = function()
-		local augroup = vim.api.nvim_create_augroup("GoFormat", {})
-
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = "*.go",
-			callback = function() require("go.format").goimport() end,
-			group = augroup,
-		})
-		require("go").setup()
-	end,
+	opts = {},
 	build = function() require("go.install").update_all_sync() end,
 }
