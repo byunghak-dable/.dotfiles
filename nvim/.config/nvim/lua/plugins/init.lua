72152fbff9b571cfc0f68c80a7270e28f2a7b920
byunghak082 <byunghak@dable.io>
2023-09-30 16:55:56 +0900

    [SORT]
    s

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,7 +1,45 @@
 return {
 	{
-		"iamcco/markdown-preview.nvim",
-		ft = "markdown",
-		build = function() vim.fn["mkdp#util#install"]() end,
+		"RRethy/vim-illuminate",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			delay = 200,
+			large_file_cutoff = 2000,
+			large_file_overrides = {
+				providers = { "lsp" },
+			},
+		},
+		config = function(_, opts)
+			require("illuminate").configure(opts)
+
+			for _, suffix in pairs({ "Text", "Read", "Write" }) do
+				vim.api.nvim_set_hl(0, "IlluminatedWord" .. suffix, { bg = "#444c56" })
+			end
+		end,
+		keys = {
+			{ "]r", function() require("illuminate").goto_next_reference(false) end },
+			{ "[r", function() require("illuminate").goto_prev_reference(false) end },
+		},
+	},
+	{
+		"ggandor/leap.nvim",
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+			{ "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+		},
+		config = function() require("leap").add_default_mappings(true) end,
+	},
+	{
+		"echasnovski/mini.pairs",
+		event = "VeryLazy",
+		opts = {},
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = { "BufReadPost", "BufNewFile" },
+		main = "ibl",
+		opts = {
+			scope = { show_start = false },
+		},
 	},
 }
