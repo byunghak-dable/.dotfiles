2d5651b16a81930ee23dc39619fdf62ef6ba1452
byunghak082 <byunghak@dable.io>
2023-10-16 22:04:34 +0900

    [SORT] remove unused config

--- nvim/.config/nvim/lua/plugins/finder.lua
+++ nvim/.config/nvim/lua/plugins/finder.lua
@@ -66,12 +66,6 @@ return {
 						},
 					},
 				},
-				fzf = {
-					fuzzy = true,
-					override_generic_sorter = true,
-					override_file_sorter = true,
-					case_mode = "smart_case",
-				},
 			},
 		}
 	end,
