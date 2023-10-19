7c3d38ebd32f7e7d25ff9ac30c889e1d42282b01
byunghak082 <byunghak@dable.io>
2023-10-19 23:23:35 +0900

    [UPDATE] change formatter, linter plugins
    s

---
+++ nvim/.config/nvim/lua/plugins/core/nvim-lint.lua
@@ -0,0 +1,23 @@
+return {
+	"mfussenegger/nvim-lint",
+	event = "VeryLazy",
+	opts = {
+		events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+		linters_by_ft = {
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+			svelte = { "eslint_d" },
+		},
+	},
+	config = function(_, opts)
+		local lint = require("lint")
+
+		lint.linters_by_ft = opts.linters_by_ft
+		vim.api.nvim_create_autocmd(opts.events, {
+			group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+			callback = function() lint.try_lint() end,
+		})
+	end,
+}
