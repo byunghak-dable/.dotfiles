cfe3ed96e23a78316901b521bf18d3d78a3f63e7
byunghak082 <byunghak@dable.io>
2023-10-21 00:22:30 +0900

    [UPDATE] onedark colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,7 @@ return {
 	{
 		"AlexvZyl/nordic.nvim",
 		event = "UIEnter",
+		enabled = false,
 		opts = {
 			leap = { dim_backdrop = true },
 		},
@@ -9,6 +10,18 @@ return {
 			local nordic = require("nordic")
 			nordic.setup(opts)
 			nordic.load()
+		end,
+	},
+	{
+		"navarasu/onedark.nvim",
+		event = "UIEnter",
+		opts = {
+			style = "warmer",
+		},
+		config = function(_, opts)
+			local onedark = require("onedark")
+			onedark.setup(opts)
+			onedark.load()
 		end,
 	},
 }
