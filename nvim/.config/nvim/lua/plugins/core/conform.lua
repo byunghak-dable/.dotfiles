6298fb355a691ba9bc40dabaf8f76085f2d69378
byunghak082 <byunghak@dable.io>
2023-10-20 22:30:36 +0900

    [UPDATE] formatter, lint config
    s

--- nvim/.config/nvim/lua/plugins/core/conform.lua
+++ nvim/.config/nvim/lua/plugins/core/conform.lua
@@ -1,26 +1,28 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	opts = {
-		formatters_by_ft = {
-			javascript = { "prettier" },
-			typescript = { "prettier" },
-			javascriptreact = { "prettier" },
-			typescriptreact = { "prettier" },
-			svelte = { "prettier" },
-			css = { "prettier" },
-			html = { "prettier" },
-			json = { "prettier" },
-			yaml = { "prettier" },
-			markdown = { "prettier" },
-			lua = { "stylua" },
-			python = { "black" },
-			go = { "goimports", "gofumpt" },
-			rust = { "rustfmt" },
-			java = { "google-java-format" },
-		},
-		format_on_save = {
-			async = false,
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			formatters_by_ft = {
+				javascript = { { { "prettierd", "prettier" } } },
+				typescript = { { "prettierd", "prettier" } },
+				javascriptreact = { { "prettierd", "prettier" } },
+				typescriptreact = { { "prettierd", "prettier" } },
+				svelte = { { "prettierd", "prettier" } },
+				css = { { "prettierd", "prettier" } },
+				html = { { "prettierd", "prettier" } },
+				json = { { "prettierd", "prettier" } },
+				yaml = { { "prettierd", "prettier" } },
+				markdown = { { "prettierd", "prettier" } },
+				lua = { "stylua" },
+				python = { "black" },
+				go = { "goimports", "gofumpt" },
+				rust = { "rustfmt" },
+				java = { "google-java-format" },
+			},
+			format_on_save = {
+				async = false,
+			},
 		},
 	},
 }
