ccd3031dbd0abba3a279d4c80cc23e5f75847254
byunghak082 <byunghak@dable.io>
2023-10-20 20:01:34 +0900

    [FIX] add event

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,14 @@
 return {
-	"AlexvZyl/nordic.nvim",
-	opts = {
-		leap = { dim_backdrop = true },
+	{
+		"AlexvZyl/nordic.nvim",
+		event = "UIEnter",
+		opts = {
+			leap = { dim_backdrop = true },
+		},
+		config = function(_, opts)
+			local nordic = require("nordic")
+			nordic.setup(opts)
+			nordic.load()
+		end,
 	},
-	config = function(_, opts)
-		local nordic = require("nordic")
-		nordic.setup(opts)
-		nordic.load()
-	end,
 }
