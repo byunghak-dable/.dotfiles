fee73cbd1f10a7cd197cd60bed8c5db3ce4c47aa
byunghak082 <byunghak@dable.io>
2023-10-22 02:47:37 +0900

    [UPDATE] add mason auto ensure install feature

--- nvim/.config/nvim/lua/plugins/core/mason.lua
+++ nvim/.config/nvim/lua/plugins/core/mason.lua
@@ -1,6 +1,17 @@
 return {
 	"williamboman/mason.nvim",
 	cmd = "Mason",
+	build = ":MasonUpdate",
 	lazy = true,
-	opts = {},
+	config = function(_, opts)
+		local mason = require("mason")
+		local registry = require("mason-registry")
+
+		mason.setup(opts)
+
+		for _, tool in ipairs(opts.ensure_installed) do
+			local p = registry.get_package(tool)
+			if not p:is_installed() then p:install() end
+		end
+	end,
 }
