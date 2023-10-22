e84ec2a435e257183f1cbe3c4a05edcf45405eae
byunghak082 <byunghak@dable.io>
2023-10-22 11:38:51 +0900

    [FIX] nvim-lint ignore errors

--- nvim/.config/nvim/lua/plugins/core/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/core/nvim-lint.lua
@@ -16,8 +16,7 @@ return {
 
 		lint.linters_by_ft = opts.linters_by_ft
 		vim.api.nvim_create_autocmd(opts.events, {
-			group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
-			callback = function() lint.try_lint() end,
+			callback = function() lint.try_lint(nil, { ignore_errors = true }) end,
 		})
 	end,
 }
