e3c9b1d336cc2747927fe3759337d07f65451fb3
byunghak082 <byunghak@dable.io>
2023-10-25 22:48:56 +0900

    [UPDATE] add additional gitsign keymaps

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -18,7 +18,12 @@ return {
 				local buf_opts = { buffer = bufnr }
 				local gs = package.loaded.gitsigns
 
+				vim.keymap.set("n", "]h", gs.next_hunk, buf_opts)
+				vim.keymap.set("n", "[h", gs.prev_hunk, buf_opts)
+				vim.keymap.set("n", "<leader>gp", gs.preview_hunk, buf_opts)
 				vim.keymap.set("n", "<leader>gb", gs.toggle_current_line_blame, buf_opts)
+				vim.keymap.set("n", "<leader>gB", function() gs.blame_line({ full = true }) end, buf_opts)
+				vim.keymap.set("n", "<leader>gr", gs.reset_buffer, buf_opts)
 			end,
 		},
 	},
