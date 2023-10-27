a17f8d78a9bf6357d0e1e9176e0dfc1a7c4801b5
byunghak082 <byunghak@dable.io>
2023-10-28 00:48:53 +0900

    [SORT]

--- nvim/.config/nvim/lua/plugins/language/json.lua
+++ nvim/.config/nvim/lua/plugins/language/json.lua
@@ -1,11 +1,14 @@
 return {
 	"neovim/nvim-lspconfig",
-	opts = {
-		settings = {
-			json = {
-				schemas = require("schemastore").json.schemas(),
-				validate = { enable = true },
+	dependencies = "b0o/schemastore.nvim",
+	opts = function(_, opts)
+		return vim.tbl_deep_extend("force", opts, {
+			settings = {
+				json = {
+					schemas = require("schemastore").json.schemas(),
+					validate = { enable = true },
+				},
 			},
-		},
-	},
+		})
+	end,
 }
--- nvim/.config/nvim/lua/plugins/language/yaml.lua
+++ nvim/.config/nvim/lua/plugins/language/yaml.lua
@@ -1,10 +1,13 @@
 return {
 	"neovim/nvim-lspconfig",
-	opts = {
-		settings = {
-			yaml = {
-				schemas = require("schemastore").yaml.schemas(),
+	dependencies = "b0o/schemastore.nvim",
+	opts = function(_, opts)
+		return vim.tbl_deep_extend("force", opts, {
+			settings = {
+				yaml = {
+					schemas = require("schemastore").yaml.schemas(),
+				},
 			},
-		},
-	},
+		})
+	end,
 }
