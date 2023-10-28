a6d376af332e346c1b4631387a39de2f1f2a2560
byunghak082 <byunghak@dable.io>
2023-10-28 21:32:00 +0900

    [UPDATE] add disable formatting
    s

--- nvim/.config/nvim/lua/plugins/core/conform.lua
+++ nvim/.config/nvim/lua/plugins/core/conform.lua
@@ -6,8 +6,29 @@ return {
 			css = { { "prettierd", "prettier" } },
 			html = { { "prettierd", "prettier" } },
 		},
-		format_on_save = {
-			async = false,
-		},
 	},
+	config = function(_, opts)
+		local conform = require("conform")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+
+		conform.setup(opts)
+
+		vim.api.nvim_create_user_command(
+			"DisableFormatting",
+			function() vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 }) end,
+			{ nargs = 0 }
+		)
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(args)
+				local bufnr = args.buf
+
+				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = augroup,
+					buffer = bufnr,
+					callback = function() conform.format({ bufnr = bufnr }) end,
+				})
+			end,
+		})
+	end,
 }
