42723d9d1d679f6e07efdb424e2886237be8b8dc
byunghak082 <byunghak@dable.io>
2023-11-25 11:21:17 +0900

    [SORT] add disabled

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
-  -- { "folke/trouble.nvim", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
+  { "trouble.nvim", enabled = false },
+  { "bufferline.nvim", enabled = false },
+  { "neo-tree.nvim", enabled = false },
+  { "nvim-notify", enabled = false },
   { "mini.pairs", enabled = false },
 }
