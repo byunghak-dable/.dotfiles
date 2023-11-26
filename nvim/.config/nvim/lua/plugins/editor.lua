68dceff244002fc850fb79d082cee5b49da5aa89
byunghak082 <byunghak@dable.io>
2023-11-26 22:52:01 +0900

    [FIX] remove barbecue

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,5 +1,19 @@
 return {
   {
+    "noice.nvim",
+    opts = {
+      presets = {
+        bottom_search = false,
+        command_palette = false,
+      },
+    },
+  },
+  {
+    "smjonas/inc-rename.nvim",
+    lazy = true,
+    opts = {},
+  },
+  {
     "numToStr/Navigator.nvim",
     keys = {
       { "<A-k>", "<cmd>NavigatorUp<cr>", mode = { "n", "t" }, desc = "Navigate Up" },
