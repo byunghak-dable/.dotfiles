b3b0b84c144304e045216c6fcb71347c433b77b9
byunghak082 <byunghak@dable.io>
2023-11-27 20:38:11 +0900

    [UPDATE] add json autocmd

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,7 +1,17 @@
+-- Change working dir
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
     local path = vim.fn.expand("%")
 
     if vim.fn.isdirectory(path) ~= 0 then vim.fn.chdir(path) end
+  end,
+})
+
+-- Fix conceallevel for json & help files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "json", "jsonc" },
+  callback = function()
+    vim.wo.spell = false
+    vim.wo.conceallevel = 0
   end,
 })
--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,16 +1,18 @@
+local opts = { noremap = true, silent = true }
+
 -- vertical center
-vim.keymap.set("n", "<C-d>", "m'<C-d>zz")
-vim.keymap.set("n", "<C-u>", "m'<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<C-d>", "m'<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "m'<C-u>zz", opts)
+vim.keymap.set("n", "n", "nzzzv", opts)
+vim.keymap.set("n", "N", "Nzzzv", opts)
 -- indent
-vim.keymap.set("v", "<", "<gv")
-vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
 -- move lines
-vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv", { silent = true })
-vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv", { silent = true })
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv", opts)
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv", opts)
 -- editor
-vim.keymap.set("v", "p", '"_dP') -- paste
-vim.keymap.set("n", "<leader>d", '"_d')
-vim.keymap.set("n", "J", "mzJ`z") -- move line up
-vim.keymap.set("n", "<C-a>", "ggVG") -- select all
+vim.keymap.set("v", "p", '"_dP', opts) -- paste
+vim.keymap.set("n", "<leader>d", '"_d', opts)
+vim.keymap.set("n", "J", "mzJ`z", opts) -- move line up
+vim.keymap.set("n", "<C-a>", "ggVG", opts) -- select all
