22e42ccabbd76561094ae42452767b75fa81142e
byunghak082 <byunghak@dable.io>
2023-11-27 21:39:29 +0900

    [UPDATE] add treesitter swap config

---
+++ nvim/.config/nvim/lua/plugins/core/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-treesitter",
+  opts = {
+    textobjects = {
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<leader>a"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<leader>A"] = "@parameter.inner",
+        },
+      },
+    },
+  },
+}
