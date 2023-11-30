fda6f974ff7fe7f34d8eb1e54666a97d803bce14
byunghak082 <byunghak@dable.io>
2023-11-30 14:15:17 +0900

    [DEL] remove ts keymaps

--- nvim/.config/nvim/lua/plugins/lang/typescript.lua
+++ nvim/.config/nvim/lua/plugins/lang/typescript.lua
@@ -9,44 +9,6 @@ return {
             importModuleSpecifierEnding = "minimal",
           },
         },
-        keys = {
-          {
-            "<leader>cO",
-            function()
-              vim.lsp.buf.code_action({
-                apply = true,
-                context = { only = { "source.organizeImports.ts" }, diagnostics = {} },
-              })
-            end,
-            desc = "Organize Imports",
-          },
-          {
-            "<leader>cR",
-            function()
-              vim.lsp.buf.code_action({
-                apply = true,
-                context = {
-                  only = { "source.removeUnused.ts" },
-                  diagnostics = {},
-                },
-              })
-            end,
-            desc = "Remove Unused",
-          },
-          {
-            "<leader>cM",
-            function()
-              vim.lsp.buf.code_action({
-                apply = true,
-                context = {
-                  only = { "source.addMissingImports.ts" },
-                  diagnostics = {},
-                },
-              })
-            end,
-            desc = "Add Missig Imports",
-          },
-        },
       },
     },
   },
