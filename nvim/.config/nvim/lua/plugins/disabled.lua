8dd7ace57ca2993398036bc1ae7a0e5a7c23f9e5
byunghak082 <byunghak@dable.io>
2023-12-05 22:52:26 +0900

    [UPDATE] enable nvim-notify

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -2,7 +2,6 @@ return {
   { "bufferline.nvim", enabled = false },
   { "dashboard-nvim", enabled = false },
   { "nvim-spectre", enabled = false },
-  { "nvim-notify", enabled = false },
   { "vim-startuptime", enabled = false },
   { "trouble.nvim", enabled = false },
   { "neo-tree.nvim", enabled = false },
