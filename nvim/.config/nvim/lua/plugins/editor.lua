203cf2dc397370eb6f7c3e9b257f61dc713df540
byunghak082 <byunghak@dable.io>
2023-12-13 19:34:25 +0900

    [UPDATE] add treesitter textobjects

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -3,6 +3,16 @@ return {
     "nvim-treesitter",
     opts = {
       textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
         swap = {
           enable = true,
           swap_next = {
