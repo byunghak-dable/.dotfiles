5884bbef71e0351eeb4a27d6c5511d6d3be2ec2c
widcraft <forsythia3240@gmail.com>
2024-01-01 01:34:52 +0900

    [UPDATE] add linux wakeup sehll scripts

---
+++ hypr/.config/hypr/scripts/wakeup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+declare -a devices=(XHC0)  # targets to disable
+
+for device in "${devices[@]}"; do
+    if grep -qw ^$device.*enabled /proc/acpi/wakeup; then
+        sudo sh -c "echo $device > /proc/acpi/wakeup"
+    fi
+done
