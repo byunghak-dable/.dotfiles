239af11779028067093e7b9341ecc89b93798bd1
byunghak082 <byunghak@dable.io>
2024-02-01 23:10:56 +0900

    [DEL] remove git worktree

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -62,22 +62,22 @@ return {
         },
       },
     },
-    config = function(_, opts)
-      local telescope = require("telescope")
-      local fb_utils = require("telescope._extensions.file_browser.utils")
-
-      for _, name in pairs({ "rename_buf", "rename_dir_buf" }) do
-        local rename_func = fb_utils[name]
-
-        fb_utils[name] = function(old_path, new_path)
-          rename_func(old_path, new_path)
-          require("lazyvim.util").lsp.on_rename(old_path, new_path)
-        end
-      end
-
-      telescope.setup({ extensions = { file_browser = opts } })
-      telescope.load_extension("file_browser")
-    end,
+    -- config = function(_, opts)
+    --   local telescope = require("telescope")
+    --   local fb_utils = require("telescope._extensions.file_browser.utils")
+    --
+    --   for _, name in pairs({ "rename_buf", "rename_dir_buf" }) do
+    --     local rename_func = fb_utils[name]
+    --
+    --     fb_utils[name] = function(old_path, new_path)
+    --       rename_func(old_path, new_path)
+    --       require("lazyvim.util").lsp.on_rename(old_path, new_path)
+    --     end
+    --   end
+    --
+    --   telescope.setup({ extensions = { file_browser = opts } })
+    --   telescope.load_extension("file_browser")
+    -- end,
   },
   {
     "debugloop/telescope-undo.nvim",
@@ -86,17 +86,5 @@ return {
       { "<leader>su", "<cmd>Telescope undo<cr>", desc = "Undo History" },
     },
     config = function() require("telescope").load_extension("undo") end,
-  },
-  {
-    "ThePrimeagen/git-worktree.nvim",
-    dependencies = "telescope.nvim",
-    keys = {
-      { "<leader>fw", "<cmd>Telescope git_worktree git_worktrees<cr>", desc = "Git Wortrees" },
-      { "<leader>fW", "<cmd>Telescope git_worktree create_git_worktree<cr>", desc = "Create Git Worktree" },
-    },
-    config = function(_, opts)
-      require("git-worktree").setup(opts)
-      require("telescope").load_extension("git_worktree")
-    end,
   },
 }
