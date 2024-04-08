9830130163239121636bd2c03ee932f106b864a4
byunghak082 <byunghak@dable.io>
2024-04-08 12:09:44 +0900

    [UPDATE] add go lint

---
+++ nvim/.config/nvim/lua/plugins/language/go.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts) table.insert(opts.ensure_installed, "golangci-lint") end,
+  },
+  {
+    "nvim-lint",
+    opts = {
+      linters_by_ft = {
+        go = { "golangcilint" },
+      },
+    },
+  },
+}
