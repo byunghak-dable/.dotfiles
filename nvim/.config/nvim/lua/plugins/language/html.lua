f57518baa298d8a63ac6a6df774a3b5aff97b8dd
byunghak082 <byunghak@dable.io>
2024-05-02 15:08:46 +0900

    [UPDATE] add html lsp config

---
+++ nvim/.config/nvim/lua/plugins/language/html.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts) vim.list_extend(opts.ensure_installed, { "htmlhint", "html-lsp", "css-lsp" }) end,
+  },
+  {
+    "nvim-lint",
+    opts = {
+      linters_by_ft = {
+        html = { "htmlhint" },
+      },
+    },
+  },
+}
