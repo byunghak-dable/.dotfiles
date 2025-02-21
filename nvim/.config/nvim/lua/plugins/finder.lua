4a7f59421e5831ea71e6fe29f5699330fc4d1029
byunghak-dable <byunghak@dable.io>
2025-02-21 15:46:34 +0900

    [DEL] remove telescope config

--- nvim/.config/nvim/lua/plugins/finder.lua
+++ nvim/.config/nvim/lua/plugins/finder.lua
@@ -21,49 +21,4 @@ return {
       filesystem = { hijack_netrw_behavior = "disabled" },
     },
   },
-  {
-    "telescope.nvim",
-    keys = {
-      { "<leader><space>", "<leader>fF", remap = true, desc = "Find Files (cwd)" },
-      { "<leader>/", "<leader>sG", remap = true, desc = "Grep (cwd)" },
-      { "<leader>fu", "<leader>fb", remap = true, desc = "Buffers" },
-    },
-    opts = {
-      defaults = {
-        sorting_strategy = "ascending",
-        layout_strategy = "flex",
-        layout_config = {
-          prompt_position = "top",
-          width = 0.9,
-          height = 0.9,
-          preview_cutoff = 120,
-        },
-        preview = { treesitter = false },
-        mappings = {
-          n = {
-            l = "select_default",
-            ["<leader>gc"] = false,
-          },
-          i = {
-            ["<C-l>"] = "select_default",
-            ["<C-j>"] = "move_selection_next",
-            ["<C-k>"] = "move_selection_previous",
-            ["<A-d>"] = "delete_buffer",
-            ["<ESC>"] = "close",
-          },
-        },
-      },
-      pickers = {
-        find_files = { follow = true },
-      },
-    },
-  },
-  {
-    "debugloop/telescope-undo.nvim",
-    dependencies = "telescope.nvim",
-    keys = {
-      { "<leader>su", "<cmd>Telescope undo<cr>", desc = "Undo History" },
-    },
-    config = function() require("telescope").load_extension("undo") end,
-  },
 }
