66cb3b2bfc2358a75701f5ea7ee0a08217680ea0
byunghak-dable <byunghak@dable.io>
2025-02-25 13:53:51 +0900

    [UPDATE] replace telescope with fzf-lua

--- nvim/.config/nvim/lua/plugins/finder.lua
+++ nvim/.config/nvim/lua/plugins/finder.lua
@@ -21,4 +21,25 @@ return {
       filesystem = { hijack_netrw_behavior = "disabled" },
     },
   },
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      { "<leader><space>", "<leader>fF", remap = true, desc = "Find Files (cwd)" },
+      { "<leader>/", "<leader>sG", remap = true, desc = "Grep (cwd)" },
+      { "<leader>fu", "<leader>fb", remap = true, desc = "Buffers" },
+    },
+    opts = function(_, opts)
+      local fzf = require("fzf-lua")
+      local config = fzf.config
+
+      opts.winopts.width = 0.9
+      opts.winopts.height = 0.9
+      opts.files.actions = {}
+
+      config.defaults.keymap.fzf["ctrl-d"] = "preview-page-down"
+      config.defaults.keymap.fzf["ctrl-u"] = "preview-page-up"
+      config.defaults.keymap.builtin["<c-d>"] = "preview-page-down"
+      config.defaults.keymap.builtin["<c-u>"] = "preview-page-up"
+    end,
+  },
 }
