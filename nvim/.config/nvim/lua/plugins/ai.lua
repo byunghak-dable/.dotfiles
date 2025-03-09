b141ff5a70a4d36743a8b8a662439d7e5bb90edb
byunghak-dable <byunghak@dable.io>
2025-03-09 10:05:47 +0900

    [UPDATE] add Avante.nvim

---
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    build = "make",
+    opts = {
+      provider = "copilot",
+    },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "echasnovski/mini.icons",
+      "zbirenbaum/copilot.lua",
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+      {
+        "folke/which-key.nvim",
+        opts = {
+          spec = {
+            {
+              mode = { "n", "v" },
+              { "<leader>a", group = "+ai", icon = { icon = "ﮧ", hl = "false" } },
+              { "<leader>a/", "<cmd>AvanteAsk<cr>", desc = "ask", icon = { icon = "", hl = "false" } },
+              { "<leader>ar", "<cmd>AvanteRefresh<cr>", desc = "refresh", icon = { icon = "", hl = "false" } },
+            },
+          },
+        },
+      },
+    },
+  },
+}
