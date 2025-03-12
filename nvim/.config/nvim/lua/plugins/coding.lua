e36b0ac762ab9f44c0009a9866373b23a7cbc3e5
byunghak-dable <byunghak@dable.io>
2025-03-12 12:47:21 +0900

    [UPDATE] add avante source to cmp

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -22,14 +22,21 @@ return {
   },
   {
     "saghen/blink.cmp",
-    opts = {
-      keymap = {
-        ["<C-j>"] = { "select_next", "fallback" },
-        ["<C-k>"] = { "select_prev", "fallback" },
-        ["<C-u>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-d>"] = { "scroll_documentation_down", "fallback" },
-        ["<C-f>"] = { "select_and_accept" },
-      },
+    dependencies = {
+      "Kaiser-Yang/blink-cmp-avante",
     },
+    opts = function(_, opts)
+      opts.keymap["<C-j>"] = { "select_next", "fallback" }
+      opts.keymap["<C-k>"] = { "select_prev", "fallback" }
+      opts.keymap["<C-u>"] = { "scroll_documentation_up", "fallback" }
+      opts.keymap["<C-d>"] = { "scroll_documentation_down", "fallback" }
+      opts.keymap["<C-f>"] = { "select_and_accept" }
+
+      table.insert(opts.sources.default, 1, "avante")
+      opts.sources.providers.avante = {
+        module = "blink-cmp-avante",
+        name = "Avante",
+      }
+    end,
   },
 }
