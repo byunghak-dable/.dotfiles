cd5ac99d917bcb3ee1fe24689d7175f542c81e25
byunghak-dable <byunghak@dable.io>
2025-03-12 13:37:42 +0900

    [DEL] remove keymap of selecting up & down

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -26,8 +26,6 @@ return {
       "Kaiser-Yang/blink-cmp-avante",
     },
     opts = function(_, opts)
-      opts.keymap["<C-j>"] = { "select_next", "fallback" }
-      opts.keymap["<C-k>"] = { "select_prev", "fallback" }
       opts.keymap["<C-u>"] = { "scroll_documentation_up", "fallback" }
       opts.keymap["<C-d>"] = { "scroll_documentation_down", "fallback" }
       opts.keymap["<C-f>"] = { "select_and_accept" }
