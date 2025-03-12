540aba585ff0957ed8397b62570f2fb103972319
byunghak-dable <byunghak@dable.io>
2025-03-12 13:37:26 +0900

    [FIX] keybinding to navigate between splits & pane

--- nvim/.config/nvim/lua/plugins/util.lua
+++ nvim/.config/nvim/lua/plugins/util.lua
@@ -15,35 +15,14 @@ return {
     opts = {},
   },
   {
-    "mrjones2014/smart-splits.nvim",
+    "numToStr/Navigator.nvim",
     keys = {
-      {
-        "<A-k>",
-        mode = { "n", "t" },
-        function() require("smart-splits").move_cursor_up() end,
-      },
-      {
-        "<A-j>",
-        function() require("smart-splits").move_cursor_down() end,
-        mode = { "n", "t" },
-        desc = "Navigator Down",
-      },
-      {
-        "<A-h>",
-        function() require("smart-splits").move_cursor_left() end,
-        mode = { "n", "t" },
-        desc = "Navigator Left",
-      },
-      {
-        "<A-l>",
-        function() require("smart-splits").move_cursor_right() end,
-        desc = "Navigator Right",
-      },
-      {
-        "<A-p>",
-        function() require("smart-splits").move_cursor_previous() end,
-        desc = "Navigator Previous",
-      },
+      { "<C-k>", "<cmd>NavigatorUp<cr>", mode = { "n", "t" }, desc = "Navigate Up" },
+      { "<C-j>", "<cmd>NavigatorDown<cr>", mode = { "n", "t" }, desc = "Navigator Down" },
+      { "<C-h>", "<cmd>NavigatorLeft<cr>", mode = { "n", "t" }, desc = "Navigator Left" },
+      { "<C-l>", "<cmd>NavigatorRight<cr>", mode = { "n", "t" }, desc = "Navigator Right" },
+      { "<C-p>", "<cmd>NavigatorPrevious<cr>", mode = { "n", "t" }, desc = "Navigator Previous" },
     },
+    opts = { disable_on_zoom = true },
   },
 }
