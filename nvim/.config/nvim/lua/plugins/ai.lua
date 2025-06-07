b8dab52dd8611a233c3796d4d91cc75f71d65132
byunghak-dable <byunghak@dable.io>
2025-06-07 14:59:57 +0900

    [UPDATE] replace avante to codecompanion

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -1,41 +1,15 @@
 return {
   {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    build = "make",
-    opts = {
-      provider = "copilot",
+    "olimorris/codecompanion.nvim",
+    keys = {
+      { "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", desc = "Code Companion Chat" },
+      { "<leader>aa", "<cmd>CodeCompanionActions<cr>", desc = "Code Companion Actions" },
     },
     dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "echasnovski/mini.icons",
-      "zbirenbaum/copilot.lua",
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
+      "nvim-treesitter/nvim-treesitter",
+      "ravitemer/mcphub.nvim",
+      { "MeanderingProgrammer/render-markdown.nvim", ft = { "codecompanion" } },
       {
         "folke/which-key.nvim",
         opts = {
@@ -48,5 +22,28 @@ return {
         },
       },
     },
+    opts = {
+      strategies = {
+        chat = { adapter = "copilot", model = "claude-sonnet-4-20250514" },
+        inline = { adapter = "copilot", model = "claude-sonnet-4-20250514" },
+        cmd = { adapter = "copilot", model = "claude-sonnet-4-20250514" },
+      },
+      extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            show_result_in_chat = true,
+            make_vars = true,
+            make_slash_commands = true,
+          },
+        },
+      },
+    },
+  },
+  {
+    "ravitemer/mcphub.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    build = "npm install -g mcp-hub@latest",
+    opts = {},
   },
 }
