1af9b6f98e8140048717fdf749e25626bc151e77
byunghak-dable <byunghak@dable.io>
2025-06-07 15:19:34 +0900

    [UPDATE] use fidget to show progress of codecompanion

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -3,12 +3,37 @@ return {
     "olimorris/codecompanion.nvim",
     keys = {
       { "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", desc = "Code Companion Chat" },
-      { "<leader>aa", "<cmd>CodeCompanionActions<cr>", desc = "Code Companion Actions" },
+      { "<leader>aa", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "Code Companion Actions" },
     },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
       "ravitemer/mcphub.nvim",
+      {
+        "j-hui/fidget.nvim",
+        opts = function()
+          local progress = require("fidget.progress")
+          local handle = nil
+
+          vim.api.nvim_create_autocmd({ "User" }, {
+            pattern = "CodeCompanionRequest*",
+            group = vim.api.nvim_create_augroup("CodeCompanionFidgetHooks", {}),
+            callback = function(request)
+              if request.match == "CodeCompanionRequestStarted" then
+                handle = progress.handle.create({
+                  title = " Requesting assistance",
+                  lsp_client = { name = "CodeCompanion Processing" },
+                })
+              elseif request.match == "CodeCompanionRequestFinished" then
+                if handle and type(handle.finish) == "function" then
+                  handle:finish()
+                  handle = nil
+                end
+              end
+            end,
+          })
+        end,
+      },
       { "MeanderingProgrammer/render-markdown.nvim", ft = { "codecompanion" } },
       {
         "folke/which-key.nvim",
