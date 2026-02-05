24f6958f9c42d5f238a760c828752a970b6eb294
byunghak-dable <byunghak@dable.io>
2026-02-05 16:00:35 +0900

    [UPDATE] fix theme

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       opts = {
-        colorscheme = "catppuccin-frappe",
+        colorscheme = "tokyonight-moon",
         news = {
           lazyvim = true,
         },
