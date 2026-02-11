54b0b199f983e8b1b2a93458d8a832c2c199fc51
byunghak-dable <byunghak@dable.io>
2026-02-12 01:55:03 +0900

    [UPDATE] add obsidian in markdown config

---
+++ nvim/.config/nvim/lua/plugins/language/markdown.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "obsidian-nvim/obsidian.nvim",
+    ft = "markdown",
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/vaults/personal",
+        },
+        {
+          name = "work",
+          path = "~/vaults/work",
+        },
+      },
+    },
+  },
+}
